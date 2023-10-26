640fa1032866af995522be26e6eee56ade265962
abbyyT <abitesfahunn@gmail.com>
2023-10-26 21:06:54 +0300

    Add files via upload

---
+++ pages/view_render.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <!-- <head>
+        <title> #Particular Project Name</title>
+        <script>
+            //Plugin Start
+            //##
+            //Plugin End 
+        </script>
+    </head> -->
+
+    <body>
+        Render Page is still under development. Your Financial and Mental contributions will go a long way to boost this feature. Thank you.
+    
+    </body>
+
+</html>
